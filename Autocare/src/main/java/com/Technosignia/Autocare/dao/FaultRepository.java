9e637be65dd3aab2ce6d36936e40c5ce93020763
radhe7 <105058197+radhe7@users.noreply.github.com>
2022-12-07 18:08:41 +0530

    Delete Autocare directory

--- Autocare/src/main/java/com/Technosignia/Autocare/dao/FaultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Technosignia.Autocare.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Technosignia.Autocare.entity.Fault;
-
-public interface FaultRepository extends JpaRepository<Fault, Long> {
-
-}
--- Autocare/src/main/java/com/Technosignia/Autocare/dao/JobcardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Technosignia.Autocare.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Technosignia.Autocare.entity.Jobcard;
-
-public interface JobcardRepository extends JpaRepository<Jobcard, Long> {
-
-}
